9dfecd0050a9d8dce4ae800f2ff9bd0a492b08b6
Luizgpp <luizgpp@gmail.com>
2015-11-24 23:55:42 -0200

    Incluindo group_concat na busca das vagas

--- application/models/extvaga_model.php
+++ application/models/extvaga_model.php
@@ -3,9 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Extvaga_model extends CI_Model 
 { 
 	public function consultaVagasNaoAprovadas(){
-		$this->db->distinct();
-		$this->db->select('v.id, titulo, numero_vagas, c.sigla AS "sigla_curso", ci.nome as cidade, u.sigla');
-		$this->db->from("vagas v");
+
+		$this->db->select('v.id, titulo, numero_vagas, group_concat(c.sigla) AS "sigla_curso", ci.nome as cidade, u.sigla');
+		$this->db->from('vagas v');
 		$this->db->join('cursos_vagas cv', 'v.id = cv.vagas_id');
 		$this->db->join('cursos c', 'c.id = cv.cursos_id');
 		$this->db->join('empresas e', 'e.id = v.empresas_id');
@@ -13,8 +13,9 @@ class Extvaga_model extends CI_Model
 		$this->db->join('uf u', 'u.id = ci.uf_id');
 		$this->db->where('v.aprovado=', 'false');
 		$this->db->group_by('v.id');
-		$query = $this->db->get();
-		return $query->result();
+		$query1 = $this->db->get();
+
+		return $query1->result();	
 	}
 
 	public function aprovarVaga($idVaga){
