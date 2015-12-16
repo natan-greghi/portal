787ffdeb84eba58477962b7f88786a60e3956e9f
natan-greghi <natan.greghi@gmail.com>
2015-12-16 21:49:55 -0200

    Modal
    inserção do modal, e programação da inscrição do aluno em uma vaga de
    estágio (parcialmente completo, falta o email)

--- application/views/cliente/principal.php
+++ application/views/cliente/principal.php
@@ -61,9 +61,11 @@
 				</div>
 
 				<div class="vaga-descri">
-					<h3><?php echo $vaga['titulo'] ?></h3>
-					<spam><?php echo $vaga['nome'] ?> - <?php echo $vaga['sigla'] ?></spam>
-				</div>
+					<a href="#" idvaga ="<?php echo $vaga['id']?>" data-toggle="modal" class="modal1" data-target="#myModal">
+						<h3><?php echo $vaga['titulo'] ?></h3>
+						<spam><?php echo $vaga['nome'] ?> - <?php echo $vaga['sigla'] ?></spam>
+					</div>
+				</a>
 				<spam class="num-vagas"><?php echo $vaga['numero_vagas']; ?></spam>
 			</div>
 			<?php }$i++;} ?>
@@ -80,9 +82,12 @@
 					</div>
 
 					<div class="vaga-descri">
-						<h3><?php echo $vaga['titulo'] ?></h3>
-						<spam><?php echo $vaga['nome'] ?> - <?php echo $vaga['sigla'] ?></spam>
-					</div>
+
+						<a href="#" idvaga ="<?php echo $vaga['id']?>" data-toggle="modal" class="modal1" id="idModal" data-target="#myModal">
+							<h3><?php echo $vaga['titulo'] ?></h3>
+							<spam><?php echo $vaga['nome'] ?> - <?php echo $vaga['sigla'] ?></spam>
+						</div>
+					</a>
 					<spam class="num-vagas"><?php echo $vaga['numero_vagas']; ?></spam>
 				</div>
 				<?php }$i++;}?>
@@ -91,3 +96,73 @@
 			<!-- fim coluna vagas 2 -->
 		</div>
 	</div>
+
+	<div id="myModal" class="modal fade" role="dialog">
+		<div class="modal-dialog">
+
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h3 class="modal-title">Vaga de Estágio</h3>
+				</div>
+				<div class="modal-body">
+					<label for="titulo"> Titulo da Vaga: </label>
+					<p id="titulo" ></p>
+					<label for="cidade"> Cidade: </label>
+					<p id="cidade"></p>
+					<label for="estado"> Estado: </label>
+					<p id="estado"></p>
+					<label for="nVagas"> Numero de Vagas: </label>
+					<p id="nVagas"></p>
+					<label for="desc"> Descrição do estágio: </label>
+					<p id="desc"></p>
+					<label for="requisitos"> Requisitos: </label>
+					<p id="requisitos"></p>
+					<label for="remuneracao"> Valor da Remuneração: </label>
+					<p id="remuneracao"> </p>
+					<label for="benificios"> Beneficios: </label>
+					<p id="benificios"></p>
+					<label for="outrosbenificios"> Outros Beneficios: </label>
+					<p id="outrosbenificios"></p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+					<a href="" id="link"><button type="button" class="btn btn-default">Candidatar-se</button></a>
+				</div>
+			</div>
+
+		</div>
+	</div>
+	<script>
+		$(document).ready(function(){
+			$('.modal1').click(function (){
+				var idvaga = $(this).attr('idvaga');
+				
+				$.ajax({
+					url: "<?php echo base_url(); ?>Principal/modal",
+					type: 'POST',
+					data: {idvaga: idvaga},					
+					success: function(result){
+						var vaga = $.parseJSON(result);						
+						
+						$('.modal-body #titulo').text(vaga[0].titulo);
+						$('.modal-body #nVagas').text(vaga[0].numero_vagas);
+						$('.modal-body #desc').text(vaga[0].descricao);
+						$('.modal-body #requisitos').text(vaga[0].requisito);
+						$('.modal-body #remuneracao').text(vaga[0].valor_bolsa);
+						$('.modal-body #benificios').text(vaga[0].Beneficio);
+						$('.modal-body #outrosbenificios').text(vaga[0].outros_beneficios);
+						$('.modal-body #cidade').text(vaga[0].nome);
+						$('.modal-body #estado').text(vaga[0].sigla); 
+						$('.modal-footer #link').attr('href','<?php echo site_url("Principal/candidatar");?>'+ "/" + idvaga + "/" + 3);
+
+					},
+					error: function(result){
+						console.log("erro");
+					}
+
+				});
+			});    
+		});
+</script>
